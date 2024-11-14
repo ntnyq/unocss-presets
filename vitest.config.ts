2eb356e8198881911eb36505009724aa3dd2cc8a
ntnyq <ntnyq13@gmail.com>
2024-11-15 03:48:20 +0800

    feat: bump unocss to v0.64.0

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    reporters: 'dot',
+    reporters: ['dot'],
     coverage: {
       reporter: ['lcov', 'text', 'json'],
     },
