8b45860dce670675e73bc6f767544f89fd4aaf89
ntnyq <ntnyq13@gmail.com>
2025-09-15 10:59:43 +0800

    chore: update

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    reporters: ['dot'],
-    coverage: {
-      include: ['**/packages/*/src/**/*.ts'],
-      reporter: ['lcov', 'text'],
-    },
+    watch: false,
   },
 })
